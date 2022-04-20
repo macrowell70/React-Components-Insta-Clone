4b75459dfe095f64499e064c3e01863af1c7e8e9
unknown <macrowell@yahoo.com>
2022-04-19 21:35:13 -0400

    got posts to show on site

--- src/App.js
+++ src/App.js
@@ -14,8 +14,8 @@ import dummyData from './dummy-data';
 import './App.css';
 
 const App = () => {
-  const [Posts, setPosts] = useState(dummyData)
-  const [SearchData, setSearchData] = useState('')
+  const [posts, setPosts] = useState(dummyData)
+  const [searchData, setSearchData] = useState('')
   // Create a state called `posts` to hold the array of post objects, **initializing to dummyData**.
   // This state is the source of truth for the data inside the app. You won't be needing dummyData anymore.
   // To make the search bar work (which is stretch) we'd need another state to hold the search term.
@@ -32,19 +32,22 @@ const App = () => {
         - if the `id` of the post matches `postId`, return a new post object with the desired values (use the spread operator).
         - otherwise just return the post object unchanged.
      */
-    setPosts(Posts.map(post => {
+  setPosts(Posts.map(post => {
       if (post.id === postId) {
-        return {...post, likes: post.likes + 1};
+        return {...post, likes: post.likes};
       } else {
         return post;
       }
     }))
+
   };
 
   return (
     <div className='App'>
       {/* Add SearchBar and Posts here to render them */}
       {/* Check the implementation of each component, to see what props they require, if any! */}
+      <SearchBar />
+      <Posts likePost={likePost} posts={posts} />
     </div>
   );
 };
